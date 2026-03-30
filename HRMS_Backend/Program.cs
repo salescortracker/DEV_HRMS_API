afa8ea19cacdeb67cf8c2b05a6c5759a9e184320
Karishma0116 <karishma@cortracker360.com>
2026-03-30 09:25:54 +0530

    Assets-Karishma_API
    Assets-Karishma_API

--- HRMS_Backend/Program.cs
+++ HRMS_Backend/Program.cs
@@ -102,6 +102,9 @@ builder.Services.AddScoped<IDashboardService, DashboardService>();
 builder.Services.AddScoped<ISubscriptionPlanService, SubscriptionPlanService>();
 builder.Services.AddScoped<IUserSubscriptionService, UserSubscriptionService>();
 builder.Services.AddScoped<IGradeService, GradeService>();
+builder.Services.AddScoped<IAssetTypeService, AssetTypeService>();
+builder.Services.AddScoped<IAssetCategoryService, AssetCategoryService>();
+builder.Services.AddScoped<ICurrencyService, CurrencyService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
