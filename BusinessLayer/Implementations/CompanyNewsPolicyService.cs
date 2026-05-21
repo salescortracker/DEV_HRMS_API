745ee6f8cfe8c30df629d445f3ff6ca389090ce6
praveennani360 <praveen@cortracker360.com>
2026-05-21 17:21:51 +0530

    CompanyPolicy_AdminScreen_Api
    CompanyPolicy_AdminScreen_Api

--- BusinessLayer/Implementations/CompanyNewsPolicyService.cs
+++ BusinessLayer/Implementations/CompanyNewsPolicyService.cs
@@ -354,7 +354,8 @@ namespace BusinessLayer.Implementations
 
             if (entity == null)
                 throw new Exception("Policy not found");
-
+            entity.CompanyId = dto.CompanyId;
+            entity.RegionId = dto.RegionId;
             entity.PolicyTitle = dto.PolicyTitle;
             entity.PolicyDescription = dto.PolicyDescription;
             entity.PostedDate = dto.PostedDate;
