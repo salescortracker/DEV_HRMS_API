ec6b7933e81893abcdbeab483c392c9bec981a82
Kittu-Mobiles <krishna@cortracker360.com>
2026-07-01 22:33:08 +0530

    Nnotification Kittu

---
+++ BusinessLayer/DTOs/NotificationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTOs
+{
+    public class NotificationDto
+    {
+        public string Type { get; set; }   
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public int UserId { get; set; }
+    }
+}
