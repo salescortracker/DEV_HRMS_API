afd58ec4d45ef19ad7406ba9a4975f30a2698bbf
KishoreSetti <kishore@cortracker360.com>
2026-07-07 14:20:42 +0530

    KPIBindingDEPTDESIG
    KPIBindingDEPTDESIG

--- BusinessLayer/Implementations/UserService .cs
+++ BusinessLayer/Implementations/UserService .cs
@@ -328,13 +328,13 @@ namespace BusinessLayer.Implementations
                     var userData = await (
         from u in _context.Users
         join r in _context.RoleMasters on u.RoleId equals r.RoleId
-       // join reg in _context.Regions on u.RegionId equals reg.RegionId
-        //join c in _context.Companies on u.CompanyId equals c.CompanyId
-        //join d in _context.Departments on u.DepartmentId equals d.DepartmentId into deptJoin
-        //from d in deptJoin.DefaultIfEmpty()
-        //join des in _context.Designations
-        //    on u.DesignationId equals des.DesignationId into desJoin
-        //from des in desJoin.DefaultIfEmpty()
+        join reg in _context.Regions on u.RegionId equals reg.RegionId
+        join c in _context.Companies on u.CompanyId equals c.CompanyId
+        join d in _context.Departments on u.DepartmentId equals d.DepartmentId into deptJoin
+        from d in deptJoin.DefaultIfEmpty()
+        join des in _context.Designations
+            on u.DesignationId equals des.DesignationId into desJoin
+        from des in desJoin.DefaultIfEmpty()
 
 
         join rm in _context.Users on u.ReportingTo equals rm.UserId into managerJoin
@@ -349,8 +349,8 @@ namespace BusinessLayer.Implementations
             u.FullName,
 
             RoleName = r.RoleName,
-           // RegionName = reg.RegionName,
-            //CompanyName = c.CompanyName,
+            RegionName = reg.RegionName,
+            CompanyName = c.CompanyName,
 
             roleId = u.RoleId,
             companyId = u.CompanyId,
@@ -358,13 +358,13 @@ namespace BusinessLayer.Implementations
             employeeCode = u.EmployeeCode,
 
             DepartmentId = u.DepartmentId,
-          //  DepartmentName = d.DepartmentName, // 🔥 STRING
+            DepartmentName = d.DepartmentName, // 🔥 STRING
 
             ReportingManagerId = u.ReportingTo,
             ReportingManagerName = rm.FullName, // 🔥 STRING
 
             DesignationId = u.DesignationId,
-           // DesignationName = des.DesignationName,
+            DesignationName = des.DesignationName,
 
 
             personalEmail = u.Email,
